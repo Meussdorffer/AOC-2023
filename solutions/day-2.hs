4fc1ecc7c11efea187f922f3f97ac70c125608a6
Meussdorffer, Jack Charles <jmeussdorffer3@gatech.edu>
2023-12-04 18:59:32 +0000

    day2

--- solutions/day-2.hs
+++ solutions/day-2.hs
@@ -1,43 +1,59 @@
+-- Run this with cabal run -- day-x "input filepath"
+-- Example: cabal run -- day-1 "./input/day-1.example"
+
 module Main where
 
 import System.Environment (getArgs)
-import Data.ByteString qualified as B
+import Data.List.Split
+import qualified Data.Map as Map
 
-{- Types for your input and your solution
+gameId :: String -> Integer
+gameId = read . last . words . head . splitOn ":"
 
-- Input    should as the type of your input parameter. AOC, typically uses arrays, matrices or complex data structures. 
-- Solution should be the type of your solution. Typically is an Int, but It can be other things, like a list of numbers
-         or a list of characters
--}
-type Input    = B.ByteString  -- default to Bytestring, but very likely you'll need to change it
-type Solution = Int
+parsePair :: [String] -> (Integer, String)
+parsePair [x, y] = (read x, y)
+parsePair _      = (0, "")
 
--- | parser transforms a raw bytestring (from your ./input/day-X.input) to your Input type. 
---   this is intended to use attoparsec for such a transformation. You can use Prelude's 
---   String if it fit better for the problem
-parser :: B.ByteString -> Input
-parser = undefined
+gameSets :: String -> [(Integer, String)]
+gameSets gamestr = 
+    let parts = splitOn ": " gamestr
+        setstr = last parts
+        setGroups = splitOn "; " setstr
+        parseGroup grp = map (parsePair . splitOn " ") (splitOn ", " grp)
+    in concatMap parseGroup setGroups
 
--- | The function which calculates the solution for part one
-solve1 :: Input -> Solution
-solve1 = error "Part 1 Not implemented"
+-- Returns True if pair is valid based on possible cube quantities, else False.
+pairCheck :: (Integer, String) -> Bool
+pairCheck (x, y)
+    | y == "red"    = x <= 12
+    | y == "green"  = x <= 13
+    | y == "blue"   = x <= 14
+    | otherwise     = False
 
--- | The function which calculates the solution for part two
-solve2 :: Input -> Solution
-solve2 = error "Part 2 Not implemented"
+-- Returns the game ID if game is valid, else 0.
+validGame :: String -> Integer
+validGame str = 
+    let gid = gameId str
+        pairs = gameSets str
+        invalidPairs = filter (not . pairCheck) pairs
+    in if (length invalidPairs) > 0 then 0 else gid
+
+solve1 :: [String] -> Integer
+solve1 = sum . map validGame
+
+cubePower :: String -> Integer
+cubePower str =
+    let pairs = gameSets str
+        grpByColor = Map.toList $ Map.fromListWith (++) $ map (\(v,k) -> (k,[v])) pairs
+        maxQtys = map (maximum . snd) grpByColor
+    in product maxQtys
+
+solve2 :: [String] -> Integer
+solve2 = sum . map cubePower
 
 main :: IO ()
 main = do
-  -- run this with cabal run -- day-x <part-number> <file-to-solution>
-  -- example: cabal run -- day-3 2 "./input/day-3.example"
-  -- will run part two of day three with input file ./input/day-3.example
-  [part, filepath] <- getArgs
-  input <- parser <$> B.readFile filepath -- use parser <$> readFile filepath if String is better
-  if read @Int part == 1
-    then do
-      putStrLn "solution to problem 1 is:"
-      print $ solve1 input
-    else do
-      putStrLn "solution to problem 2 is:"
-      print $ solve2 input
-
+    [filepath] <- getArgs
+    input <- readFile filepath >>= return . lines
+    putStr "part 1 solution: " >> print (solve1 input)
+    putStr "part 2 solution: " >> print (solve2 input)
