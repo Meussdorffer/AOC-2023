13b05a375a7c91f74359860385188994c8b97962
Meussdorffer, Jack Charles <jmeussdorffer3@gatech.edu>
2023-12-04 03:04:54 +0000

    day-1

--- solutions/day-1.hs
+++ solutions/day-1.hs
@@ -1,43 +1,41 @@
+-- Run this with cabal run -- day-x <part-number> <file-to-solution>
+-- Example: cabal run -- day-3 2 "./input/day-3.example"
+
 module Main where
 
 import System.Environment (getArgs)
-import Data.ByteString qualified as B
+import Data.Char (isDigit)
+import Data.List
 
-{- Types for your input and your solution
+firstAndLast :: String -> String
+firstAndLast [x] = [x] ++ [x]
+firstAndLast xs = [head xs] ++ [last xs]
 
-- Input    should as the type of your input parameter. AOC, typically uses arrays, matrices or complex data structures. 
-- Solution should be the type of your solution. Typically is an Int, but It can be other things, like a list of numbers
-         or a list of characters
--}
-type Input    = B.ByteString  -- default to Bytestring, but very likely you'll need to change it
-type Solution = Int
+solve1 :: String -> Integer
+solve1 = sum . map (read . firstAndLast . filter isDigit) . lines
 
--- | parser transforms a raw bytestring (from your ./input/day-X.input) to your Input type. 
---   this is intended to use attoparsec for such a transformation. You can use Prelude's 
---   String if it fit better for the problem
-parser :: B.ByteString -> Input
-parser = undefined
+numbers :: [String]
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
--- | The function which calculates the solution for part one
-solve1 :: Input -> Solution
-solve1 = error "Part 1 Not implemented"
+substrToNum :: String -> String
+substrToNum str = case elemIndex True (map (\x -> isPrefixOf x str) numbers) of
+    Just n -> show n
+    Nothing -> if isDigit (head str) then [(head str)] else ""
 
--- | The function which calculates the solution for part two
-solve2 :: Input -> Solution
-solve2 = error "Part 2 Not implemented"
+parseString :: String -> String
+parseString = intercalate "" . map (substrToNum) . filter (/= "") . tails
+
+solve2 :: String -> Integer
+solve2 = sum . map (read . firstAndLast . parseString) . lines
 
 main :: IO ()
 main = do
-  -- run this with cabal run -- day-x <part-number> <file-to-solution>
-  -- example: cabal run -- day-3 2 "./input/day-3.example"
-  -- will run part two of day three with input file ./input/day-3.example
-  [part, filepath] <- getArgs
-  input <- parser <$> B.readFile filepath -- use parser <$> readFile filepath if String is better
-  if read @Int part == 1
+    [part, filepath] <- getArgs
+    input <- readFile filepath
+    if read @Int part == 1
     then do
-      putStrLn "solution to problem 1 is:"
-      print $ solve1 input
+        putStrLn "solution to problem 1 is:"
+        print $ solve1 input
     else do
-      putStrLn "solution to problem 2 is:"
-      print $ solve2 input
-
+        putStrLn "solution to problem 2 is:"
+        print $ solve2 input
