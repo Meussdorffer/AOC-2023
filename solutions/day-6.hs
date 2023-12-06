e7c84f26eb7c7286cc576bb2d79050e00e1b4431
Meussdorffer, Jack Charles <jmeussdorffer3@gatech.edu>
2023-12-06 17:30:18 +0000

    day 6 init

---
+++ solutions/day-6.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import System.Environment (getArgs)
+import Data.List.Split
+
+-- solve1 :: [String] -> Int
+-- solve1 input = 
+
+-- solve2 :: [String] -> Int
+-- solve2 input = 
+
+main :: IO ()
+main = do
+    [filepath] <- getArgs
+    -- input <- readFile filepath >>= return . splitOn "\n\n"
+    -- putStr "part 1 solution: " >> print (solve1 input)
+    -- putStr "part 2 solution: " >> print (solve2 input)
